79aac51824c1c5a780cfa5c45e8e9e670843fba3
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-03-18 21:24:06 +0900

    [chore] next-auth 환경설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,21 +4,24 @@ import type { AppProps } from 'next/app'
 import globalStyles from '@styles/globalStyles'
 import Layout from '@shared/Layout'
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
+import { SessionProvider } from 'next-auth/react'
 
 const client = new QueryClient({})
 
 export default function App({
   Component,
-  pageProps: { dehydratedState, ...pageProps },
+  pageProps: { dehydratedState, session, ...pageProps },
 }: AppProps) {
   return (
     <Layout>
       <Global styles={globalStyles} />
-      <QueryClientProvider client={client}>
-        <Hydrate state={dehydratedState}>
-          <Component {...pageProps} />
-        </Hydrate>
-      </QueryClientProvider>
+      <SessionProvider session={session}>
+        <QueryClientProvider client={client}>
+          <Hydrate state={dehydratedState}>
+            <Component {...pageProps} />
+          </Hydrate>
+        </QueryClientProvider>
+      </SessionProvider>
     </Layout>
   )
 }
