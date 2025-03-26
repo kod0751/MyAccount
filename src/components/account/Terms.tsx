aa0503950bfbca746d10012387b33007e8c70179
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-03-26 17:38:13 +0900

    [feat] 약관동의 기능구현

--- src/components/account/Terms.tsx
+++ src/components/account/Terms.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
+import { MouseEvent, useState } from 'react'
 
 import { TERMSLIST } from '@constants/account'
 import { Term } from '@models/account'
 import Agreement from '@shared/Agreement'
-import FixedBottomButton from '../shared/FixedBottomButton'
+import dynamic from 'next/dynamic'
+
+const FixedBottomButton = dynamic(() => import('@shared/FixedBottomButton'), {
+  ssr: false,
+})
 
 export default function Terms() {
   const [termsAgreements, setTermsAgreements] = useState(() =>
@@ -11,12 +15,32 @@ export default function Terms() {
   )
   console.log('termsAgreements', termsAgreements)
 
-  const handleAllAgreement = () => {}
+  const handleAgreement = (id: string, checked: boolean) => {
+    setTermsAgreements((prevTerms) => {
+      return prevTerms.map((term) =>
+        term.id === id ? { ...term, checked } : term,
+      )
+    })
+  }
+
+  const handleAllAgreement = (_: MouseEvent<HTMLElement>, checked: boolean) => {
+    setTermsAgreements((prevTerms) => {
+      return prevTerms.map((term) => ({ ...term, checked }))
+    })
+  }
+
+  const allCheckedTerms = termsAgreements.every((term) => term.checked)
+  const essCheckedTerms = termsAgreements
+    .filter((term) => term.mandatory)
+    .every((term) => term.checked)
 
   return (
     <div>
       <Agreement>
-        <Agreement.Title checked={false} onChange={handleAllAgreement}>
+        <Agreement.Title
+          checked={allCheckedTerms}
+          onChange={handleAllAgreement}
+        >
           약관 모두 동의
         </Agreement.Title>
         {termsAgreements.map((term) => (
@@ -24,13 +48,17 @@ export default function Terms() {
             key={term.id}
             link={term.link}
             checked={term.checked}
-            onChange={() => {}}
+            onChange={(_, checked) => handleAgreement(term.id, checked)}
           >
             {term.mandatory ? '[필수]' : '[선택]'} {term.title}
           </Agreement.Description>
         ))}
       </Agreement>
-      <FixedBottomButton label="약관동의" disabled={false} onClick={() => {}} />
+      <FixedBottomButton
+        label="약관동의"
+        disabled={essCheckedTerms === false}
+        onClick={() => {}}
+      />
     </div>
   )
 }
