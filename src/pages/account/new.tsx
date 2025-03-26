5dc041d441b296351bbd3bb08a412fe6a14500c6
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-03-26 19:09:59 +0900

    [feat] 계좌계설시 이미 데이터가 있는 경우 없는 부분부터 시작

--- src/pages/account/new.tsx
+++ src/pages/account/new.tsx
@@ -1,18 +1,21 @@
 import { useState } from 'react'
+import { getSession } from 'next-auth/react'
+import { GetServerSidePropsContext } from 'next'
 
 import withAuth from '@hooks/withAuth'
 import ProgressBar from '@components/shared/ProgressBar'
 import Terms from '@components/account/Terms'
 import useUser from '@hooks/useUser'
-import { setTerms } from '@remote/account'
+import { getTerms, setTerms } from '@remote/account'
+import { User } from '@models/user'
 
 // STEP 0 = 약관동의
 // STEP 1 = 계좌 개설 폼 페이지
 // STEP 2 = 완료페이지
 const LAST_STEP = 2 // 완료페이지
 
-function AccountNew() {
-  const [step, setStep] = useState(0)
+function AccountNew({ initialStep }: { initialStep: number }) {
+  const [step, setStep] = useState(initialStep)
   const user = useUser()
 
   return (
@@ -30,6 +33,34 @@ function AccountNew() {
       ) : null}
     </div>
   )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession(context)
+
+  const agreedTerms = await getTerms((session?.user as User).id)
+
+  if (agreedTerms == null) {
+    return {
+      props: {
+        initialStep: 0,
+      },
+    }
+  }
+
+  if (agreedTerms != null) {
+    return {
+      props: {
+        initialStep: 1,
+      },
+    }
+  }
+
+  return {
+    props: {
+      initialStep: 0,
+    },
+  }
 }
 
 export default withAuth(AccountNew)
--- src/remote/account.ts
+++ src/remote/account.ts
@@ -1,4 +1,4 @@
-import { collection, doc, setDoc } from 'firebase/firestore'
+import { collection, doc, getDoc, setDoc } from 'firebase/firestore'
 
 import { COLLECTIONS } from '@constants/collection'
 import { store } from '@remote/firebase'
@@ -14,4 +14,19 @@ export function setTerms({
     userId,
     termIds,
   })
+}
+
+export async function getTerms(userId: string) {
+  const snapshot = await getDoc(
+    doc(collection(store, COLLECTIONS.TERMS), userId),
+  )
+
+  if (snapshot.exists() === false) {
+    return null
+  }
+
+  return {
+    id: snapshot.id,
+    ...(snapshot.data() as { userId: string; termIds: string[] }),
+  }
 }
