f2e9369134d305ccb1ad594e7b452bd633407d00
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-03-28 15:46:53 +0900

    [feat] 계좌계설 부분 UI 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
 import dynamic from 'next/dynamic'
+import { getSession } from 'next-auth/react'
+import { dehydrate, QueryClient } from 'react-query'
+import { GetServerSidePropsContext } from 'next'
 
 import Account from '@components/home/Account'
 import { BannerSkeleton } from '@components/home/EventBanners'
 import { CreditScoreSkeleton } from '@components/home/CreditScore'
 import { CardListSkeleton } from '@components/home/CardList'
-import Spacing from '@/components/shared/Spacing'
+import Spacing from '@components/shared/Spacing'
+import { User } from '@models/user'
+import { getAccount } from '@remote/account'
 
 const EventBanners = dynamic(() => import('@components/home/EventBanners'), {
   ssr: false,
@@ -32,4 +37,26 @@ export default function Home() {
       <CardList />
     </>
   )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getSession(context)
+
+  if (session != null && session.user != null) {
+    const client = new QueryClient()
+
+    await client.prefetchQuery(['account', (session.user as User).id], () =>
+      getAccount((session.user as User).id),
+    )
+
+    return {
+      props: {
+        dehydratedState: JSON.parse(JSON.stringify(dehydrate(client))),
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
 }
