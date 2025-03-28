94a8efad8dcf0ea585be6e22e5ac2f86f0562d90
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-03-28 19:30:26 +0900

    [feat] 입출금 테스트 데이터 추가

--- src/components/test/TransactionForm.tsx
+++ src/components/test/TransactionForm.tsx
@@ -5,6 +5,9 @@ import TextField from '@shared/TextField'
 import Spacing from '@shared/Spacing'
 import Select from '@shared/Select'
 import Button from '@shared/Button'
+import { getAccount, updateAccountBalance } from '@remote/account'
+import { createTransaction } from '@remote/transaction'
+import { Transaction } from '@models/transaction'
 
 export default function TransactionForm() {
   const [formValues, setFormValues] = useState({
@@ -24,6 +27,47 @@ export default function TransactionForm() {
   }
 
   console.log('formValues', formValues)
+
+  const handleSubmit = async () => {
+    const account = await getAccount(formValues.userId)
+
+    if (account == null) {
+      window.alert('해당 유저는 계좌를 보유하고 있지 않습니다.')
+      return
+    }
+
+    // 출금 (유저 잔액 5000 - 20000 => -15000)
+    if (
+      formValues.type === 'withdraw' &&
+      account.balance - Number(formValues.amount) < 0
+    ) {
+      window.alert(
+        `지금 유저의 잔액은 ${account.balance}원 입니다. 잔액이 부족합니다.`,
+      )
+      return
+    }
+
+    const balance =
+      formValues.type === 'withdraw'
+        ? account.balance - Number(formValues.amount)
+        : account.balance + Number(formValues.amount)
+
+    const newTransaction = {
+      ...formValues,
+      amount: Number(formValues.amount),
+      date: new Date().toISOString(),
+      balance,
+    } as Transaction
+
+    console.log('newTransaction', newTransaction)
+
+    await Promise.all([
+      createTransaction(newTransaction),
+      updateAccountBalance(formValues.userId, balance),
+    ])
+
+    window.alert('입출금 데이터 생성 완료')
+  }
 
   return (
     <div>
@@ -65,7 +109,9 @@ export default function TransactionForm() {
           onChange={handleFormValues}
         />
         <Spacing size={8} />
-        <Button>{formValues.type === 'deposit' ? '입금' : '출금'}</Button>
+        <Button onClick={handleSubmit}>
+          {formValues.type === 'deposit' ? '입금' : '출금'}
+        </Button>
       </Flex>
     </div>
   )
