be9b312eab893c4e930b9fce178eb4ff3fd33472
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-04-01 20:25:45 +0900

    [feat] 월별차트 데이터 및 적용

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -2,14 +2,37 @@ import dynamic from 'next/dynamic'
 
 import withAuth from '@hooks/withAuth'
 
+const MonthlyChart = dynamic(() => import('@components/account/MonthlyChart'))
 const Transactions = dynamic(() => import('@components/account/Transactions'))
 
 function AccountPage() {
   return (
     <div>
+      <MonthlyChart chartData={generateMonthlyChartData()} />
       <Transactions />
     </div>
   )
+}
+
+function generateMonthlyChartData() {
+  return [
+    '2025-01-31',
+    '2025-02-28',
+    '2025-03-31',
+    '2025-04-30',
+    '2025-05-31',
+    '2025-06-30',
+    '2025-07-31',
+    '2025-08-31',
+    '2025-09-30',
+    '2025-10-31',
+    '2025-11-30',
+    '2025-12-31',
+  ].map((date) => ({
+    date,
+    balance: Math.floor(Math.random() * (1000000 - 10000 + 1)) + 10000,
+    // 월별 랜덤값 입력
+  }))
 }
 
 export default withAuth(AccountPage)
