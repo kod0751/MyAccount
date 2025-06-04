a2e781476d253c418cbe613858a9fe1387471764
kod0751 <50727457+kod0751@users.noreply.github.com>
2025-06-04 18:52:46 +0900

    [refactor] withAuth에서 담당하는 부분 제거

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import globalStyles from '@styles/globalStyles'
 import Layout from '@shared/Layout'
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query'
 import { SessionProvider } from 'next-auth/react'
-import AuthGuard from '@/components/auth/AuthGuard'
 import Navbar from '@/components/shared/NavBar'
 import { AlertContextProvider } from '@/contexts/AlertContext'
 
@@ -22,10 +21,8 @@ export default function App({
         <QueryClientProvider client={client}>
           <Hydrate state={dehydratedState}>
             <AlertContextProvider>
-              <AuthGuard>
-                <Navbar />
-                <Component {...pageProps} />
-              </AuthGuard>
+              <Navbar />
+              <Component {...pageProps} />
             </AlertContextProvider>
           </Hydrate>
         </QueryClientProvider>
